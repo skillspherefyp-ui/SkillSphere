44ff79b1e8d91be722659501956132b8acaca671
skillspherefyp-ui <skillspherefyp@gmail.com>
2026-03-18 20:24:27 +0500

    Update SuperAdminNavigator.js

--- AppAndroidSS/src/navigation/SuperAdminNavigator.js
+++ AppAndroidSS/src/navigation/SuperAdminNavigator.js
@@ -7,13 +7,13 @@ import CourseListScreen from '../screens/admin/CourseListScreen';
 import CreateCourseScreen from '../screens/admin/CreateCourseScreen';
 import CourseDetailScreen from '../screens/admin/CourseDetailScreen';
 import AddTopicsScreen from '../screens/admin/AddTopicsScreen';
+import GenerationLogsScreen from '../screens/admin/GenerationLogsScreen';
 import StudentListScreen from '../screens/admin/StudentListScreen';
 import StudentDetailScreen from '../screens/admin/StudentDetailScreen';
 import CategoryManagementScreen from '../screens/admin/CategoryManagementScreen';
 import CertificateManagementScreen from '../screens/admin/CertificateManagementScreen';
 import FeedbackScreen from '../screens/admin/FeedbackScreen';
 import ForgotPasswordScreen from '../screens/auth/ForgotPasswordScreen';
-import AILectureReviewScreen from '../screens/shared/AILectureReviewScreen';
 
 const Stack = createStackNavigator();
 
@@ -33,13 +33,13 @@ const SuperAdminNavigator = () => {
       <Stack.Screen name="CreateCourse" component={CreateCourseScreen} />
       <Stack.Screen name="CourseDetail" component={CourseDetailScreen} />
       <Stack.Screen name="AddTopics" component={AddTopicsScreen} />
+      <Stack.Screen name="GenerationLogs" component={GenerationLogsScreen} />
       <Stack.Screen name="Students" component={StudentListScreen} />
       <Stack.Screen name="StudentDetail" component={StudentDetailScreen} />
       <Stack.Screen name="Categories" component={CategoryManagementScreen} />
       <Stack.Screen name="CategoryManagement" component={CategoryManagementScreen} />
       <Stack.Screen name="CertificateManagement" component={CertificateManagementScreen} />
       <Stack.Screen name="Feedback" component={FeedbackScreen} />
-      <Stack.Screen name="AILectureReview" component={AILectureReviewScreen} />
       <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
